e6527d52fa0b121c39489ee8fd3efd7a090bf8c4
Alex Standiford <alex@standiford.us>
2023-10-08 00:05:30 -0400

    Adds getCacheTtl

--- lib/Interfaces/Table.php
+++ lib/Interfaces/Table.php
@@ -16,4 +16,11 @@ interface Table
      * @return string
      */
     public function getAlias(): string;
+
+    /**
+     * Gets the TTL value that should be used when caching data in this table.
+     *
+     * @return int
+     */
+    public function getCacheTtl(): int;
 }
