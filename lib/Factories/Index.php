e7f4c31de8c8449e82dc2b0464965bfbbea5ea5f
Alex Standiford <alex@standiford.us>
2023-10-14 07:55:15 -0400

    Revert "Revert "Simplifies index""
    This reverts commit 50e47a309c6efd3c2ee077a7c9a7b1d2251bacef.

--- lib/Factories/Index.php
+++ lib/Factories/Index.php
@@ -14,15 +14,11 @@ final class Index
     public function __construct(
         string $name,
         array $columns,
-        bool $isUnique = false,
-        bool $isPrimary = false,
         ?string $type = null,
         ...$attributes
     ) {
         $this->name = $name;
         $this->columns = $columns;
-        $this->isUnique = $isUnique;
-        $this->isPrimary = $isPrimary;
         $this->type = $type;
         $this->attributes = $attributes;
     }
@@ -45,26 +41,6 @@ final class Index
     public function getColumns(): array
     {
         return $this->columns;
-    }
-
-    /**
-     * Determines if the index is unique.
-     *
-     * @return bool
-     */
-    public function isUnique(): bool
-    {
-        return $this->isUnique;
-    }
-
-    /**
-     * Determines if the index is a primary key.
-     *
-     * @return bool
-     */
-    public function isPrimary(): bool
-    {
-        return $this->isPrimary;
     }
 
     /**
