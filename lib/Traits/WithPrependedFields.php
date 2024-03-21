1c58fffb391e2e57c9bac1115856d9c15b9c7af7
Alex Standiford <alex@standiford.us>
2024-03-21 10:01:46 -0400

    Add field validation in clause builder
    Added a new function, getFieldString, in ClauseBuilder.php to get the field string and filter invalid fields. A validation method, tableHasField, has also been included in WithPrependedFields.php to verify if the field exists in the table. These enhancements aim to ensure robust field handling in the database clause building stage, and to offer some extra protection against MySQL injection.

--- lib/Traits/WithPrependedFields.php
+++ lib/Traits/WithPrependedFields.php
@@ -9,6 +9,26 @@ trait WithPrependedFields
     use WithUseTable;
 
     /**
+     * Validates that the given table has a column that matches the specified field.
+     *
+     * @param string $field
+     * @param Table|null $table
+     * @return bool
+     */
+    protected function tableHasField(string $field, ?Table $table = null): bool
+    {
+        $table = $table ?? $this->table;
+
+        foreach ($table->getColumns() as $column) {
+            if ($column->getName() === $field) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * Prepends the specified field with the current table's alias.
      *
      * @param string $field
